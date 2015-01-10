93b8e1df09b8e9fa25fa8bc3febecf636ebcbf19
Ian McGowan <ian@mcgowanfam.com>
2015-01-10 17:17:20 -0500

    Check in watch.sh

---
+++ watch.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Watch command - trivial utility for when you're on AIX...
+# usage: watch.sh <your_command> <sleep_duration>
+
+if [ "$1X" == "x" ]; then
+    echo 'Usage: watch.sh <your command> <sleep duration>'
+    exit 1
+fi
+
+while :; 
+do 
+  $1
+  sleep $2
+done
