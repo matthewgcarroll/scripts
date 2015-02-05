d91f2ca172311f2a2491fcd9ea98c2020a59ee81
Ian McGowan <ian.mcgowan@gmail.com>
2015-02-04 17:34:08 -0800

    Oops, better parameter checking

--- watch.sh
+++ watch.sh
@@ -2,7 +2,7 @@
 # Watch command - trivial utility for when you're on AIX...
 # usage: watch.sh <your_command> <sleep_duration>
 
-if [ "$1X" == "x" ]; then
+if [[ "$1x" == "x" || "$2x" == "x" ]]; then
     echo 'Usage: watch.sh <your command> <sleep duration>'
     exit 1
 fi
