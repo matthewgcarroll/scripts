212dfbc8573df0dd192ceebc0f877bc39f78ea85
Ian McGowan <ian.mcgowan@gmail.com>
2016-09-24 16:06:38 -0700

    Update cash_rcvd_trial.pl

--- cash_rcvd_trial.pl
+++ cash_rcvd_trial.pl
@@ -3,15 +3,15 @@
 
 while (<>) {
   # Header line
-  #CASH.RCVD.TRIAL.RPT                                       ACCOUNT: TRINITY                                              PAGE   259
+  #CASH.RCVD.TRIAL.RPT                                       ACCOUNT: ********                                              PAGE   259
   #
   #                                                                                                   GROSS CONTRACT  CONTRACT BALANCE
   #CONTRACT NUMBER                          TOTAL RECVD   RENTAL RECVD  SALES/USE TAX   LATE CHARGES  PYMTS FROM BEG  CURR RENT RCVB
   #CUSTOMER NAME         TYPE DATE RECVD    INTEREST     MISCELLANEOUS  MISC TAX        CUR INT RCVB  PYMTS DISPOSED  REM RENT RCVB
   #---------------       ---- ----------    -----------  -------------  -------------   ------------  --------------  ----------------
   #
-  #250-0814527-001       STAN 01/26/04           749.62         713.92          35.70           0.00       34,268.16         20,703.68
-  #BSN AND BSN-SANTA FE, BERGSTRA                  0.00           0.00           0.00           0.00       13,564.48              0.00
+  #***************       STAN 01/26/04           749.62         713.92          35.70           0.00       34,268.16         20,703.68
+  #*******************E, ********                  0.00           0.00           0.00           0.00       13,564.48              0.00
   #                                                                                             0.00            0.00         20,703.68
 
   if (m#^\d\d\d-\d\d\d\d\d\d\d-\d\d\d#) {
